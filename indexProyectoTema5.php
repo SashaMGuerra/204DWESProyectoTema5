3dc672900ee0e5f6e9d885e53887310a94ac3d0f
SashaMGuerra <74042799+SashaMGuerra@users.noreply.github.com>
2021-11-24 09:09:51 +0100

    Commit inicial 24/11/2021
    Creado index, css simple y footer común a todo el proyecto.

---
+++ indexProyectoTema5.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+    Autor: Isabel Martínez Guerra
+    Fecha: 24/11/2021
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>IMG - Tema 5</title>
+        <link href="webroot/css/commonTema5.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <header>
+            <a class="volver" href="../proyectoDWES/indexProyectoDWES.php">Volver</a>
+            <h1>Desarrollo Web en Entorno Servidor</h1>
+            <h2>Proyecto Tema 5</h2>
+        </header>
+        <main>
+            
+        </main>
+        <?php include_once './codigoPHP/elementoFooter.php'; // Footer ?>
+    </body>
+</html>
