ae02d62334860196fbb1d1ddc7fed6fb4901b002
SashaMGuerra <74042799+SashaMGuerra@users.noreply.github.com>
2021-11-24 09:24:22 +0100

    Ejercicio 0 24/11/2021
    Creado únicamente el ejercicio 0.

--- indexProyectoTema5.php
+++ indexProyectoTema5.php
@@ -8,6 +8,7 @@
         <meta charset="UTF-8">
         <title>IMG - Tema 5</title>
         <link href="webroot/css/commonTema5.css" rel="stylesheet" type="text/css"/>
+        <link href="webroot/css/indexProyectoTema5.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
         <header>
@@ -16,7 +17,15 @@
             <h2>Proyecto Tema 5</h2>
         </header>
         <main>
-            
+            <table>
+                <tr>
+                    <td class="enunciado">Mostrar el contenido de las variables superglobales y phpinfo().</td>
+                    <td>
+                        <a href="codigoPHP/practica0.php"><img src="webroot/media/img/execute-icon.jpg" alt=""/></a>
+                        <a href="mostrarcodigo/mostrarCodigo0.php"><img src="webroot/media/img/doc_icon.png" alt=""/></a>
+                    </td>
+                </tr>
+            </table>
         </main>
         <?php include_once './codigoPHP/elementoFooter.php'; // Footer ?>
     </body>
---
+++ mostrarcodigo/mostrarCodigo0.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--
+    Autor: Isabel Martínez Guerra
+    Fecha: 24/11/2021
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>IMG - Mostrar ej. 0</title>
+        <link href="../webroot/css/commonTema5.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <header>
+            <?php include_once '../codigoPHP/elementoBtVolver.php'; // Botón de regreso ?>
+            <h1>Práctica 0</h1>
+        </header>
+        <main>
+            <?php
+            /*
+             * @author Isabel Martínez Guerra
+             * Fecha de creación: 24/11/2021
+             * Fecha última modificación: 24/11/2021
+             * 
+             * Mostrado de la práctica 0.
+             */
+
+            highlight_file('../codigoPHP/practica0.php');
+            ?>
+        </main>
+        <?php include_once '../codigoPHP/elementoFooter.php'; // Footer, ya formateado ?>
+        
+    </body>
+</html>
